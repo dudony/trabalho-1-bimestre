794abde554c7e998cc8ff823a102c494da4d8170
VITOR <VITOR@DESKTOP-TMR7742.home>
2017-03-19 16:12:30 -0300

    atualizado

--- Exercicio14/src/Exercicio14.java
+++ Exercicio14/src/Exercicio14.java
@@ -17,40 +17,40 @@ public class Exercicio14 {
 			n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor"));
 			n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro valor"));
 			adicao = n1 + n2;
-			JOptionPane.showConfirmDialog(null, "A valor da soma é de:  " + adicao);
+			JOptionPane.showConfirmDialog(null, "A valor da soma é de:  " + adicao);break;
 		case 2:
 			double subitracao, n3, n4;
 			n3 = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor"));
 			n4 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro valor"));
 			subitracao = n3 - n4;
-			JOptionPane.showConfirmDialog(null, "A valor da soma é de:  " + subitracao);
+			JOptionPane.showConfirmDialog(null, "A valor da soma é de:  " + subitracao);break;
 		case 3:
 			double divisao, n5, n6;
 			n5 = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor"));
 			n6 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro valor"));
 			divisao = n5 + n6;
-			JOptionPane.showConfirmDialog(null, "A valor da soma é de:  " + divisao);
+			JOptionPane.showConfirmDialog(null, "A valor da soma é de:  " + divisao);break;
 		case 4:
 			double mult, n7, n8;
 			n7 = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor"));
 			n8 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro valor"));
 			mult = n7 + n8;
-			JOptionPane.showConfirmDialog(null, "A valor da soma é de:  " + mult);
+			JOptionPane.showConfirmDialog(null, "A valor da soma é de:  " + mult);break;
 		case 5:
 			Integer fat = 0, n9 ,n = 0, i;
 			n9 = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor"));       
 			for (i = 1; i<= n; i++){
 	              fat = fat * i;
 			}
-			JOptionPane.showConfirmDialog(null,"O valor do fatorial é de: "+ fat);
+			JOptionPane.showConfirmDialog(null,"O valor do fatorial é de: "+ fat);break;
 		case 6:
 			Integer base, exp, valor;
 			base = Integer.parseInt(JOptionPane.showInputDialog("Digite a base:"));
 			exp = Integer.parseInt(JOptionPane.showInputDialog("Digite o expoente:"));
 			valor = base^exp;
-			JOptionPane.showConfirmDialog(null, "O seu valor é de: " + valor);
+			JOptionPane.showConfirmDialog(null, "O seu valor é de: " + valor);break;
 		case 7:
-			JOptionPane.showConfirmDialog(null, "Comando invalido");
+			JOptionPane.showConfirmDialog(null, "Comando invalido");break;
 		}
 	}
 }
