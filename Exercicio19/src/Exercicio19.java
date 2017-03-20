cc7f51e7b9fb0deb7259586384ea51f4a99691cd
VITOR <VITOR@DESKTOP-TMR7742.home>
2017-03-20 12:17:08 -0300

    19

---
+++ Exercicio19/src/Exercicio19.java
@@ -0,0 +1,11 @@
+public class Exercicio19 {
+
+	public static void main(String[]args){
+		Integer[] contagem = new Integer[1001];
+		 for(int i=0; i<1001; i++){
+			contagem[i] = i;
+			System.out.println(i);
+		 }
+	}
+}
+
